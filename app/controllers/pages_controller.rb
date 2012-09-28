a99706c827f84e84cbb82c30562e7315e93e8f2d
Lawrence <lawrence@lawrence.(none)>
2012-09-28 10:10:53 +0800

    Done with static pages in Rails Tutorial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home(家)"
   end
 
   def contact
+    @title = "Contact(聯絡人)"
+  end
+
+  def about
+    @title = "About(關於)"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => "Rails Tutorial Sample App | Home(家)")
     end
   end
 
@@ -13,6 +19,23 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => "Rails Tutorial Sample App | Contact(聯絡人)")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => "Rails Tutorial Sample App | About(關於)")
     end
   end
 
